1bed4f5683436429d22c3bbc102f3d14c77fdd9a
Ilian Häsler <ilian.haesler@89grad.ch>
2024-05-22 14:35:33 +0200

    fix cors headers

--- backend/testcompass_backend/settings_dev.py
+++ backend/testcompass_backend/settings_dev.py
@@ -40,6 +40,30 @@ INSTALLED_APPS = [
     'user_tests',
     'organizations',
     "rest_framework",
+    'corsheaders',
+]
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
 ]
 
 MIDDLEWARE = [
