026156c3c1540617680eb04f360dd3ccbaed7547
ian-codes <ian.kraft@hotmail.com>
2024-06-08 18:32:09 +0200

    fix backend urls

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import dotenv from 'dotenv';
 
-dotenv.config({ path: process.env.NODE_ENV === 'production' ? '.env.production' : '.env' });
+dotenv.config({ path: process.env.NODE_ENV === 'production' 
+	? 'https://test-compass.refugium-romontberg.ch/' 
+	: '127.0.0.1:8000/' });
 
 export default defineConfig({
 	plugins: [sveltekit()],
